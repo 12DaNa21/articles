399260584f5600b01010f573dd7f8d3a1d58aa7f
12DaNa21 <145206474+12DaNa21@users.noreply.github.com>
2024-10-08 21:12:56 +0300

    latest articles

---
+++ src/components/App/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ArticleList from "../ArticleList/ArticleList";
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          "https://hn.algolia.com/api/v1/search?query=react"
+        );
+        setArticles(response.data.hits);
+      } catch (error) {
+        // Встановлюємо стан error в true
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchArticles();
+  }, []);
+
+  return (
+    <div>
+      <h1>Latest articles</h1>
+      {loading && <p style={{ fontSize: 20 }}>Loading data, please wait...</p>}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
+};
+
+export default App;
